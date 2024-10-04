e786d607be53a7aa5ec31294ce70dab3e1d33fc7
neyaznak <neyaznak@gmail.com>
2024-10-05 03:19:00 +0530

    password encryption

--- src/app.js
+++ src/app.js
@@ -1,21 +1,34 @@
 const express = require("express");
 const connectDB = require("./config/database");
 const app = express();
-
 const User = require("./models/user");
+const { validateSignUpData } = require("./utils/validation");
+const bcrypt = require("bcrypt");
 
 app.use(express.json());
 
 //POST api for user sign up
 app.post("/signup", async (req, res) => {
-  //creating a new instance of the User model
-  const user = new User(req.body);
-
   try {
+    //Validation of data
+    validateSignUpData(req);
+    const { firstName, lastName, emailId, password } = req.body;
+
+    //Encrypt the password
+    const passwordHash = await bcrypt.hash(password, 10);
+    console.log(passwordHash);
+    //creating a new instance of the User model
+    const user = new User({
+      firstName,
+      lastName,
+      emailId,
+      password: passwordHash,
+    });
+
     await user.save();
     res.send("User Added Successfully!");
   } catch (err) {
-    res.status(400).send("Error saving the user:" + err.message);
+    res.status(400).send("ERROR :" + err.message);
   }
 });
 
---
+++ src/utils/validation.js
@@ -0,0 +1,14 @@
+const validator = require("validator");
+const validateSignUpData = (req) => {
+  const { firstName, lastName, emailId, password } = req.body;
+
+  if (!firstName || !lastName) {
+    throw new Error("Name is not valid!");
+  } else if (!validator.isStrongPassword(password)) {
+    throw new Error("Not a strong password!");
+  }
+};
+
+module.exports = {
+  validateSignUpData,
+};
