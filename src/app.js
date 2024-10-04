be1c2a27e9a0598df65882e0ca8c88aab40fc3b3
neyaznak <neyaznak@gmail.com>
2024-10-05 02:07:59 +0530

    api level data sanitization

--- src/app.js
+++ src/app.js
@@ -61,12 +61,24 @@ app.delete("/user", async (req, res) => {
 });
 
 //update a user
-app.patch("/user", async (req, res) => {
-  const userId = req.body.userId;
+app.patch("/user/:userId", async (req, res) => {
+  // const userId = req.body.userId;
+  const userId = req.params?.userId;
   const data = req.body;
-  console.log(data);
+
   try {
-    await User.findByIdAndUpdate({ _id: userId }, data, {
+    const ALLOWED_UPDATES = ["photoUrl", "about", "gender", "age", "skills"];
+
+    const isUpdateAllowed = Object.keys(data).every((k) =>
+      ALLOWED_UPDATES.includes(k)
+    );
+    if (!isUpdateAllowed) {
+      throw new Error("update not allowed");
+    }
+    if (data?.skills.length > 10) {
+      throw new Error("Skills can not be more than 10");
+    }
+    const user = await User.findByIdAndUpdate({ _id: userId }, data, {
       returnDocument: "after",
       runValidators: true,
     });
