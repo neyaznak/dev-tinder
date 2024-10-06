728b555a236744e1bda6238c0491b35c144c6e37
neyaznak <neyaznak@gmail.com>
2024-10-07 01:18:58 +0530

    logout and edit profile apis

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -29,28 +29,35 @@ authRouter.post("/signup", async (req, res) => {
 });
 
 authRouter.post("/login", async (req, res) => {
-    try {
-      const { emailId, password } = req.body;
-  
-      const user = await User.findOne({ emailId: emailId });
-      if (!user) {
-        throw new Error("Invalid credentials");
-      }
-      const isPasswordValid = await user.validatePassword(password);
-  
-      if (isPasswordValid) {
-        const token = await user.getJWT();
-  
-        res.cookie("token", token, {
-          expires: new Date(Date.now() + 8 * 3600000),
-        });
-        res.send("Login successful!!!");
-      } else {
-        throw new Error("Invalid credentials");
-      }
-    } catch (err) {
-      res.status(404).send("ERROR :" + err.message);
+  try {
+    const { emailId, password } = req.body;
+
+    const user = await User.findOne({ emailId: emailId });
+    if (!user) {
+      throw new Error("Invalid credentials");
     }
+    const isPasswordValid = await user.validatePassword(password);
+
+    if (isPasswordValid) {
+      const token = await user.getJWT();
+
+      res.cookie("token", token, {
+        expires: new Date(Date.now() + 8 * 3600000),
+      });
+      res.send("Login successful!!!");
+    } else {
+      throw new Error("Invalid credentials");
+    }
+  } catch (err) {
+    res.status(404).send("ERROR :" + err.message);
+  }
+});
+
+authRouter.post("/logout", async (req, res) => {
+  res.cookie("token", null, {
+    expires: new Date(Date.now()),
   });
+  res.send("Logged out!");
+});
 
 module.exports = authRouter;
